d5af56823280ca80f1a6f3bb1598543aec6e1491
Punna <punnatapat02@gmail.com>
2026-01-19 19:53:51 +0100

    Add small Kuschel-Café name generator project

---
+++ day1/day1/main.py
@@ -0,0 +1,6 @@
+# --- Das Kuschel-Café Namensgenerator ---
+print("Willkommen beim Kuschel-Café Generator! ☕🐶")
+besitzer = input("Wie heißt der Café-Inhaber?\n")
+tier = input("Wie heißt dein Haustier?\n")
+print("\n--- Dein Ergebnis ---")
+print("Dein Café heißt: " + besitzer + " & " + tier + "'s Kaffeeklatsch 🍰✨")
