7bacbab68fd9271277131c3d561a888c653bd3f9
Punna <punnatapat02@gmail.com>
2026-01-19 20:19:20 +0100

    Delete day1/day1 directory

--- day1/day1/main.py
+++
@@ -1,6 +0,0 @@
-# --- Das Kuschel-Café Namensgenerator ---
-print("Willkommen beim Kuschel-Café Generator! ☕🐶")
-besitzer = input("Wie heißt der Café-Inhaber?\n")
-tier = input("Wie heißt dein Haustier?\n")
-print("\n--- Dein Ergebnis ---")
-print("Dein Café heißt: " + besitzer + " & " + tier + "'s Kaffeeklatsch 🍰✨")
